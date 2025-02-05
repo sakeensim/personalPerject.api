201244377b313daf2ffa56c61e406ba1591b7be8
sakeensim <sakeensim@gmail.com>
2025-02-05 15:28:28 +0700

    insert

--- controllers/auth-contoller.js
+++ controllers/auth-contoller.js
@@ -1,24 +1,57 @@
 
+const prisma = require("../configs/prisma");
 const createError = require("../utils/createError")
-exports.register = (req,res,next) =>{
+const bcrypt = require("bcryptjs")
+
+
+exports.register = async (req,res,next) =>{
     //code   
     try{
     //step 1 req.body
     const {email, firstname,lastname,password, confirmpassword} = req.body
 
     //step 2 validate
-    if(!email){
-        return createError(400, "email is require")
-    }
-    if(!firstname){
-        return createError(400, "firstname is require")
-    }
+
+    //---------
+    // if(!email){
+    //     return createError(400, "email is require")
+    // }
+    // if(!firstname){
+    //     return createError(400, "firstname is require")
+    // }
+    ///-------
+
+
     //step 3 Check already
+    const checkEmail = await prisma.profile.findFirst({
+        where: {
+            email: email,
+        }
+    })
+    console.log(checkEmail)
+    if(checkEmail){
+        return createError(400,"Email is alreadr exits!!!")
+    }
     //step 4 Encrypt bcrypt
+    // const salt =bcrypt.genSaltSync(10)
+    // const hashedPassword = bcrypt.hashSync(password,salt)
+    const hashedPassword = bcrypt.hashSync(password,10)
+    // console.log(salt)
+    // console.log(hashedPassword)
+
     //step 5 Inser to DB
+    const profile = await prisma.profile.create({
+        data: {
+            email: email,
+            firstname: firstname,
+            lastname: lastname,
+            password: hashedPassword,
+        }
+    })
+
     //step 6 Response
 
-       res.json({message  : "hello register"})
+       res.json({message  : "Register Success"})
     }catch(error){
         console.log("step 2 Catch")
        next(error)
